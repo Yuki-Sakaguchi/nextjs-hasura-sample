cea3d20275f04dd924bf3586864f25456f3eb569
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-08-19 22:00:01 +0900

    Update CRUDを実行

--- pages/hasura-crud.tsx
+++ pages/hasura-crud.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, FormEvent } from 'react'
+import { FC, useState, FormEvent, ChangeEvent } from 'react'
 import { useQuery, useMutation } from '@apollo/client'
 import {
   GET_USERS,
@@ -13,8 +13,10 @@ import {
   UpdateUserMutation,
 } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
+import { UserItem } from '../components/UserItem'
 
 const HasuraCRUD: FC = () => {
+  const [editedUser, setEditedUser] = useState({ id: '', name: '' })
   const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
     fetchPolicy: 'cache-and-network',
   })
@@ -54,9 +56,73 @@ const HasuraCRUD: FC = () => {
     },
   })
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedUser.id) {
+      // idがあれば編集モード
+      try {
+        await update_users_by_pk({
+          variables: {
+            id: editedUser.id,
+            name: editedUser.name,
+          },
+        })
+      } catch (err) {
+        alert(err.message)
+      }
+      setEditedUser({ id: '', name: '' })
+    } else {
+      // idがなければ追加モード
+      try {
+        await insert_users_one({
+          variables: {
+            name: editedUser.name,
+          },
+        })
+      } catch (err) {
+        alert(err.message)
+      }
+      setEditedUser({ id: '', name: '' })
+    }
+  }
+
+  if (error) {
+    return <Layout title="Hasura CRUD">Error: {error.message}</Layout>
+  }
+
   return (
     <Layout title="Hasura CRUD">
       <p className="mb-3 font-bold">Hasura CRUD</p>
+      <form
+        className="flex flex-col justify-center items-center"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="px-3 py-2 border border-gray-300"
+          placeholder="New user ?"
+          type="text"
+          value={editedUser.name}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setEditedUser({ ...editedUser, name: e.target.value })
+          }
+        />
+        <button
+          disabled={!editedUser.name}
+          className="disabled:opacity-40 my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+          data-testid="new"
+          type="submit"
+        >
+          {editedUser.id ? 'Update' : 'Create'}
+        </button>
+      </form>
+      {data?.users.map((user) => (
+        <UserItem
+          key={user.id}
+          user={user}
+          delete_users_by_pk={delete_users_by_pk}
+          setEditedUser={setEditedUser}
+        />
+      ))}
     </Layout>
   )
 }
